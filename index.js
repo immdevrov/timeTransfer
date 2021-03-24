cd5774187802321467ab72db04775aa09523f868
Roman Vayser <immdevrov@gmail.com>
2021-03-24 22:16:08 +0300

    refactor: transferEntries function

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import transferEntries from './src/transferEntries.js'
 import { startFromCommandLine, RL } from './src/utils.js'
+import * as config from './.config.js'
+
+
 ;(async () => {
   const line = await startFromCommandLine().catch((e) => {
     console.error(e)
   })
-  await transferEntries(line).catch((e) => console.error(e))
+  await transferEntries(line, config).catch((e) => console.error(e))
   RL.close()
 })()
--- src/transferEntries.js
+++ src/transferEntries.js
@@ -2,36 +2,39 @@ import ToggleConnect from './classes/toggle.js'
 import Worklogs from './classes/worklogs.js'
 import JiraConnect from './classes/jiraConnect.js'
 import { today } from './utils.js'
-import {
-  dates as customDates,
-  linkToTimesheet,
-  user,
-  jiraConnectOptions,
-  toggleConnectAuth,
-} from '../.config.js'
 
-const DATES_MAP = {
-  today: today,
-  dates: customDates,
-}
-
-export default async function transferEntries(mode) {
-  const dates = DATES_MAP[mode]
-
-  if (!dates) {
-    throw new Error('Неверно определен mode')
+function getDates (mode, todayDate, customDates) {
+  if (mode === 'today') {
+    return todayDate
+  } else if (mode === 'dates') {
+    return customDates
   }
 
-  const toggleConnectInstance = new ToggleConnect(toggleConnectAuth)
+  throw new Error('Неверно определен mode')
+}
+
+
+export default async function transferEntries(mode, config) {
+  const {
+    dates: CUSTOM_DATES,
+    linkToTimesheet: LINK_TO_TIMESHEET,
+    user: USER,
+    jiraConnectOptions: JIRA_CONNECT_OPTIONS,
+    toggleConnectAuth: TOGGLE_CONNECT_AUTH,
+  } =  config
+
+  const dates = getDates(mode, today, CUSTOM_DATES)
+
+  const toggleConnectInstance = new ToggleConnect(TOGGLE_CONNECT_AUTH)
   const entries = await toggleConnectInstance.getEntries(dates)
 
   const worklogs = new Worklogs(entries)
   const validWorklogs = worklogs.getValidWorklogs()
 
   console.log(validWorklogs)
-  console.log(linkToTimesheet)
+  console.log(LINK_TO_TIMESHEET)
 
-  const jiraConnectInstance = new JiraConnect(user, jiraConnectOptions)
+  const jiraConnectInstance = new JiraConnect(USER, JIRA_CONNECT_OPTIONS)
 
   validWorklogs.map(
     async (worklog) =>
